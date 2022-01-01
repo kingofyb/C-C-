33a6920a2e3d4a53554df3c677858bf1c3959be9
kingofyb <50230192+kingofyb@users.noreply.github.com>
2022-01-01 12:16:24 +0000

    Add files via upload

---
+++ 2021_10_20/2021_10_20/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//求最大值
+//int Max(int a, int b)
+//{
+//	int max = (a > b ? a : b);
+//	return max;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int max = 0;
+//	scanf("%d %d", &num1, &num2);
+//	max = Max(num1, num2);
+//	printf("最大值为：%d\n", max);
+//	return 0;
+//}
+
+//求商求余
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int shang = a / b;
+//	int remain = a % b;
+//	printf("%d %d", shang, remain);
+//	return 0;
+//}
+
+//int main()
+//{
+//    int a = 40;
+//    int c = 212;
+//    int b = (-8 + 22) * a - 10 + c / 2;
+//    printf("%d\n", b);
+//    return 0;
+//}
