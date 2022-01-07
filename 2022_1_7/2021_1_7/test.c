418d812776a13179c79d93bc2be27a0a360aea95
kingofyb <50230192+kingofyb@users.noreply.github.com>
2022-01-07 15:18:44 +0000

    Add files via upload

---
+++ 2022_1_7/2021_1_7/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//prepare for exam week 1.10-1.13
+
+//KiKi这学期努力学习程序设计基础，要期末考试了，BoBo老师告诉他，总成绩包括四个部分，如下：
+//总成绩 = 实验成绩 * 20 % +课堂表现成绩 * 10 % +过程考核成绩 * 20 % +期末上机考试成绩 * 50 % ，
+//现在输入KiKi的各项成绩，请计算KiKi的总成绩。
+
+//int main()
+//{
+//    int a, b, c, d;
+//    scanf("%d %d %d %d", &a, &b, &c, &d);
+//    double g = 0;
+//    g = a * 0.2 + b * 0.1 + c * 0.2 + d * 0.5;
+//    printf("%.1f", g);
+//    return 0;
+//}
+
+
+//2020年来到了，KiKi要对他的n位好朋友说n遍”Happy new year!Good luck!”的祝福语。
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    for (int i = 0; i < n; i++)
+//    {
+//        printf("Happy new year!Good luck!\n");
+//    }
+//    return 0;
+//}
