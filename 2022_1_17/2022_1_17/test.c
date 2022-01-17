452445eef3164de4820fd170a2a989808efe0515
kingofyb <50230192+kingofyb@users.noreply.github.com>
2022-01-17 19:22:06 +0000

    Add files via upload

---
+++ 2022_1_17/2022_1_17/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//整数序列，删除指定数字
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[50] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int del = 0;
+//	scanf("%d", &del);
+//	int j = 0;//用来记录下一个存数组的位置
+//	for (i = 0; i < n; i++)
+//	{
+//		if (arr[i] != del)
+//		{
+//			arr[j++] = arr[i];
+//		}
+//	}
+//	for (i = 0; i < j; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//使用移位运算<<实现2的n次方计算
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", 1 << n);
+//	return 0;
+//}
+
+//指针/地址 要被存储起来，需要一个空间，这个空间就是指针变量
+
+int main()
+{
+	char* arr[] = { "abcdef","qwer","zhangsan" };
+	return 0;
+}
