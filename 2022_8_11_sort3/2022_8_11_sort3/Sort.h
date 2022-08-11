9e6e0166a1cd0933cf356bd6ca1ffc8acc415583
kingofyb <50230192+kingofyb@users.noreply.github.com>
2022-08-11 22:18:43 +0100

    Add files via upload

---
+++ 2022_8_11_sort3/2022_8_11_sort3/Sort.h
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#pragma once
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+void PrintArray(int* a, int n);
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
+void SelectSort(int* a, int n);
+void HeapSort(int* a, int n);
+void BubbleSort(int* a, int n);
+void QuickSort(int* a, int begin, int end);
+void QuickSortNonR(int* a, int begin, int end);
+
+extern int callCount;
